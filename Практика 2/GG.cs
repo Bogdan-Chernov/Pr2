3ed9e099612c65f677a5d9e8a05d9c15fd0f3e78
Bogdan-Chernov <72433961+Bogdan-Chernov@users.noreply.github.com>
2020-10-09 15:28:37 +0300

    Initial commit

---
+++ Практика 2/GG.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_2
+{
+    class GG
+    {
+        public string name = "Богдан";
+        public string firstname = "Чернов";
+        public int weight = 67;
+        public int height = 176;
+        public int years = 17;
+
+        static internal string SetName()
+        {
+            Console.WriteLine("Введите ваше имя");
+            string B = Console.ReadLine();
+            Console.WriteLine($"Ваше новое имя - {B}");
+            return B;
+        }
+        static internal string Setfirstname()
+        {
+            Console.WriteLine("Введите вашу фамилию");
+            string C = Console.ReadLine();
+            Console.WriteLine($"Ваша новая фамилия - {C}");
+            return C;
+        }
+
+        static internal int Setweight()
+        {
+            Console.WriteLine("Введите ваш вес (Кг)");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш новый вес (Кг) - {A}");
+            return A;
+        }
+        static internal int Setheight()
+        {
+            Console.WriteLine("Введите ваш рост (См)");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш новый рост (См) - {A}");
+            return A;
+        }
+
+        static internal int Setyears()
+        {
+            Console.WriteLine("Введите ваш возраст");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Ваш новый возраст - {A}");
+            return A;
+        }
+    }
+}
---
+++ Практика 2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GG Us = new GG();
+            Console.WriteLine("Ваши текущие параметры:");
+            Console.WriteLine();
+            Console.WriteLine($"Имя - {Us.name}");
+            Console.WriteLine($"Фамилия - {Us.firstname}");
+            Console.WriteLine($"Возраст - {Us.years}");
+            Console.WriteLine($"Вес (Кг) - {Us.weight}");
+            Console.WriteLine($"Рост (См) - {Us.height}");
+
+            Console.WriteLine();//Имя
+            GG.SetName();
+            Console.WriteLine();//Фамилия
+            GG.Setfirstname();
+            Console.WriteLine();//Вес
+            GG.Setweight();
+            Console.WriteLine();//Рост
+            GG.Setheight();
+            Console.WriteLine();//Лет
+            GG.Setyears();
+
+
+        }
+    }
+}
